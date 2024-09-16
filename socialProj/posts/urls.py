6cf0032281cbb4b2a32188c534340caf4a46c5d7
samer Samara <samer.w.samara@gmail.com>
2024-09-16 19:06:13 +0300

    adding the delet post button

--- socialProj/posts/urls.py
+++ socialProj/posts/urls.py
@@ -6,5 +6,6 @@ app_name = "posts"
 urlpatterns =[
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
     path("list_view/", views.PostListView.as_view(), name="list_view"),
-    path("add/", views.add_post, name="add_post")
+    path("add/", views.add_post, name="add_post"),
+    path("<int:pk>/delete/", views.delete_post, name="delete_post")
 ]
--- socialProj/posts/views.py
+++ socialProj/posts/views.py
@@ -2,7 +2,7 @@ from datetime import timezone
 from django.utils import timezone
 from django.views import generic
 from .models import Post
-from django.shortcuts import render ,redirect
+from django.shortcuts import render ,redirect , get_object_or_404
 # Create your views here.
 class PostDetailView(generic.DetailView):
     template_name = "posts/detailes.html"
@@ -33,3 +33,10 @@ def add_post (request):
         return redirect('posts:list_view')
 
     return redirect('posts:list_view')
+
+def delete_post (requset, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if requset.method == "POST":
+        post.delete()
+        return redirect('posts:list_view')
+    return redirect(requset.META.get('HTTP_REFERER','posts:list_view')) # request.META is and dictionary in django that contain the HTTP headers sent with the requset , from it there is .get(HTTP_REFERER) and that will be the url of the previous page so we can redicrct the user in the same page and if it is empty for some reason the git methode will redirect to the path we put it
