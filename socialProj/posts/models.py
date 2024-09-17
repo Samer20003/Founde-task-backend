77b9b2d5f1ac5cba3fe82c1c17d8b1ae17ccad76
samer Samara <samer.w.samara@gmail.com>
2024-09-17 08:28:24 +0300

    add a comment class and render it on the list_view

--- socialProj/posts/models.py
+++ socialProj/posts/models.py
@@ -6,3 +6,9 @@ class Post(models.Model):
     img_url = models.CharField(max_length=200)
     user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     pup_date = models.DateTimeField()
+
+class Comment(models.Model):
+    body = models.CharField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
